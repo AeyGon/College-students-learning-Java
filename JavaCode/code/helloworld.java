47c0ddda52bac6fb3b3fcd31d2715c9ce5381615
.z <168911497+AeyGon@users.noreply.github.com>
2026-04-14 18:29:23 +0800

    My first project/我的第一个项目
    记录我学习java的过程

---
+++ JavaCode/code/helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(10);
+        System.out.println(10.5);
+        System.out.println('a');
+        System.out.println(true);
+
+    }
+}
